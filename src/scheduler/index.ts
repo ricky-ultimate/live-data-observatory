c3b8fd8483aee70940303e54e153acbfb62c4f25
Ricky Ultimate <rickyogigi@gmail.com>
2026-03-11 03:41:21 +0100

    feat(scheduler): register space weather scheduler

--- src/scheduler/index.ts
+++ src/scheduler/index.ts
@@ -1,7 +1,9 @@
 import { startEarthquakeScheduler } from "../core/feeds/earthquakes/earthquake.scheduler";
+import { startSpaceWeatherScheduler } from "../core/feeds/space-weather/space-weather.scheduler";
 import logger from "../utils/logger.utils";
 
 export const startAllSchedulers = (): void => {
   startEarthquakeScheduler();
+  startSpaceWeatherScheduler();
   logger("INFO", "All schedulers started");
 };
